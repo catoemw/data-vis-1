785b12526eb24302926b3a9022a4247ed42e9476
Matt Catoe <catoemw@gmail.com>
2019-09-24 23:00:19 -0400

    Add table for loan data

--- frontend/src/routes/LoanTable.jsx
+++ frontend/src/routes/LoanTable.jsx
@@ -1,7 +1,62 @@
-import React from 'react'
+import React, { Fragment, useEffect, useState } from 'react'
+import { parse } from 'papaparse';
+import { Table, TableHead, TableBody, TablePagination, TableRow, TableCell, Typography } from "@material-ui/core";
 
 const LoanTable = () => {
-    return <div>LoanTable</div>
+    const [loanData, setLoanData] = useState(null)
+    const [rowsPerPage, setRowsPerPage] = useState(5);
+    const [page, setPage] = useState(0);
+    const getLoanData = async () => {
+        const request = new Request("http://localhost:12059/react-interview/getLoanData", {
+            method: "GET"
+        })
+        const response = await fetch(request);
+        const loanData = await response.text();
+        setLoanData(parse(loanData).data);
+    }
+    useEffect(() => {
+        getLoanData()
+    }, []);
+
+    const handleChangePage = (e, newPage) => {
+        setPage(newPage);
+    };
+    const handleChangeRowsPerPage = e => {
+        setRowsPerPage(+e.target.value);
+        setPage(0);
+    };
+    return <div style={{ padding: 16, overflow: "scroll" }}>
+        <Typography align="center" variant="h6">Loan Data</Typography>
+        {loanData &&
+            <Fragment>
+                <Table style={{ margin: "16px 12px 0px 12px", overflow: "hidden" }}>
+                    <TableHead>
+                        <TableRow>
+                            {loanData[0].map((column, index) => {
+                                return <TableCell key={`column-${index}`}>{column}</TableCell>
+                            })}
+                        </TableRow>
+                    </TableHead>
+                    <TableBody>
+                        {loanData.slice(page * rowsPerPage + 1, page * rowsPerPage + rowsPerPage).map((row, index) => (
+                            <TableRow key={index}>
+                                {row.map(data => <TableCell key={`${data}-${index}`}>{data}</TableCell>)}
+                            </TableRow>
+                        ))}
+                    </TableBody>
+                </Table>
+                <TablePagination
+                    rowsPerPageOptions={[5, 10, 25]}
+                    component="div"
+                    count={loanData.slice(1).length}
+                    rowsPerPage={rowsPerPage}
+                    page={page}
+                    onChangePage={handleChangePage}
+                    onChangeRowsPerPage={handleChangeRowsPerPage}
+                />
+            </Fragment>
+        }
+    </div>
 }
 
 export default LoanTable;
